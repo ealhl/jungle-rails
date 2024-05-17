7918da6bccba2f261efc396857b1850a86d5e7a0
ealhl <yayt.tuk@gmail.com>
2024-05-16 19:41:10 -0700

    add categories new and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
+  end
   get 'about/index'
   root to: 'products#index'
 
@@ -19,6 +24,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
